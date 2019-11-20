60c484db4500f42daca855bd026e16e3092b0b9b
chenwq <chenwq@idea-group.cn>
2019-11-20 11:33:17 +0800

    add

---
+++ client/components/crawler-program/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="crawler-program">
+
+  </div>
+</template>
+
+<script>
+	export default {
+		name: "index"
+	}
+</script>
+
+<style scoped>
+
+</style>
---
+++ client/pages/editor/components/attr-configure/attr-props-components/props-attr/index.js
@@ -0,0 +1,15 @@
+import Text from './text'
+import imageSrc from './imageSrc'
+import imageSrcList from './imageSrcList'
+import Url from './url'
+import musicSrc from './musicSrc'
+import placeholder from './placeholder'
+
+export default {
+	[Text.name]: Text,
+	[imageSrc.name]: imageSrc,
+	[Url.name]: Url,
+	[musicSrc.name]: musicSrc,
+	[imageSrcList.name]: imageSrcList,
+	[placeholder.name]: placeholder
+}
